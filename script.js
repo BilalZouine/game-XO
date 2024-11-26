9ac7d0c18c1417ae7631a7918dae71a55ef468da
BILAL ZOUINE <bilalzouine9@gmail.com>
2024-11-26 15:00:03 +0100

    change script

--- script.js
+++ script.js
@@ -1,75 +1,113 @@
 var turn = 0
-
 var sqauares = []
 var alerts = document.getElementById("alert")
+function handleClick() {
+    game(this.id);
+}
+
+document.querySelectorAll('.XO').forEach(e => {
+    e.addEventListener('click', handleClick)
+
+})
+function reload() {
+    alerts.innerText = 'play again'
+
+    document.querySelector('.container').childNodes.forEach(e => {
+        e.innerText = ''
+
+        if (e.classList) {
+            e.classList.remove('black')
+        }
+
+
+    })
+    
+    document.querySelectorAll('.XO').forEach(e => {
+        e.addEventListener('click', handleClick)
+
+    })
+}
+
 function ween(num1, num2, num3) {
     // alerts.innerText = 'WENNER'
-    document.getElementById("XO" + num1).style.background = "black"
-    document.getElementById("XO" + num2).style.background = "black"
-    document.getElementById("XO" + num3).style.background = "black"
-    alerts.innerText="😍"
-    setInterval(function () { alerts.innerText += '.' }, 1000)
-    setTimeout(function () { location.reload() }, 3000)
-    
+    document.getElementById("XO" + num1).classList.add("black")
+    document.getElementById("XO" + num2).classList.add("black")
+    document.getElementById("XO" + num3).classList.add("black")
+    alerts.innerText = "😍"
+    document.querySelectorAll('.XO').forEach(e => {
+        e.removeEventListener('click', handleClick)
+
+    })
+    var interval = setInterval(function () { alerts.innerText += '.' }, 1000)
+    setTimeout(function () {
+        reload()
+        clearInterval(interval)
+    }, 3000)
+
 }
-function BALANCE(){
-    var espace=""
-    index = sqauares.indexOf(espace)
-    if(index==-1){
-    // alerts.innerText = 'THE BALANCE'
-    alerts.innerText = '😔'
-    setInterval(function () { alerts.innerText += '.' }, 1000)
-    setTimeout(function () { location.reload() }, 6000)
+function BALANCE() {
+    var espace = ""
+
+    let index = sqauares.indexOf(espace)
+
+    if (index == -1) {
+        // alerts.innerText = 'THE BALANCE'
+        alerts.innerText = '😔'
+        let interval = setInterval(function () { alerts.innerText += '.' }, 1000)
+        setTimeout(function () {
+            reload()
+            clearInterval(interval)
+        }, 3000)
     }
 
 }
 
 function weanr(id) {
-    sqauares[1] = document.getElementById("XO1").textContent
-    sqauares[2] = document.getElementById("XO2").textContent
-    sqauares[3] = document.getElementById("XO3").textContent
-    sqauares[4] = document.getElementById("XO4").textContent
-    sqauares[5] = document.getElementById("XO5").textContent
-    sqauares[6] = document.getElementById("XO6").textContent
-    sqauares[7] = document.getElementById("XO7").textContent
-    sqauares[8] = document.getElementById("XO8").textContent
-    sqauares[9] = document.getElementById("XO9").textContent
-    
+    sqauares[0] = document.getElementById("XO1").textContent
+    sqauares[1] = document.getElementById("XO2").textContent
+    sqauares[2] = document.getElementById("XO3").textContent
+    sqauares[3] = document.getElementById("XO4").textContent
+    sqauares[4] = document.getElementById("XO5").textContent
+    sqauares[5] = document.getElementById("XO6").textContent
+    sqauares[6] = document.getElementById("XO7").textContent
+    sqauares[7] = document.getElementById("XO8").textContent
+    sqauares[8] = document.getElementById("XO9").textContent
 
 
 
-    if (sqauares[1] == sqauares[2] && sqauares[2] == sqauares[3] && sqauares[1] != '') {
+
+    if (sqauares[0] == sqauares[1] && sqauares[1] == sqauares[2] && sqauares[0] != '') {
         ween(1, 2, 3)
     }
-    else if (sqauares[4] == sqauares[5] && sqauares[5] == sqauares[6] && sqauares[4] != '') {
+    else if (sqauares[3] == sqauares[4] && sqauares[4] == sqauares[5] && sqauares[3] != '') {
         ween(4, 5, 6)
 
     }
-    else if (sqauares[7] == sqauares[8] && sqauares[8] == sqauares[9] && sqauares[7] != '') {
+    else if (sqauares[6] == sqauares[7] && sqauares[7] == sqauares[8] && sqauares[6] != '') {
         ween(7, 8, 9)
 
     }
-    else if (sqauares[1] == sqauares[4] && sqauares[4] == sqauares[7] && sqauares[1] != '') {
+    else if (sqauares[0] == sqauares[3] && sqauares[3] == sqauares[6] && sqauares[0] != '') {
         ween(1, 4, 7)
 
     }
-    else if (sqauares[2] == sqauares[5] && sqauares[5] == sqauares[8] && sqauares[2] != '') {
+    else if (sqauares[1] == sqauares[4] && sqauares[4] == sqauares[7] && sqauares[1] != '') {
         ween(2, 5, 8)
 
     }
-    else if (sqauares[3] == sqauares[6] && sqauares[6] == sqauares[9] && sqauares[3] != '') {
+    else if (sqauares[2] == sqauares[5] && sqauares[5] == sqauares[8] && sqauares[2] != '') {
         ween(3, 6, 9)
     }
-    else if (sqauares[1] == sqauares[5] && sqauares[5] == sqauares[9] && sqauares[1] != '') {
+    else if (sqauares[0] == sqauares[4] && sqauares[4] == sqauares[8] && sqauares[0] != '') {
         ween(1, 5, 9)
 
     }
-    else if (sqauares[3] == sqauares[5] && sqauares[5] == sqauares[7] && sqauares[3] != '') {
+    else if (sqauares[2] == sqauares[4] && sqauares[4] == sqauares[6] && sqauares[2] != '') {
         ween(3, 5, 7)
 
 
     }
-    else{
+    else {
         BALANCE()
     }
 }
